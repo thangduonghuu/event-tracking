c8e63e5abd89bf8702a4e342296edad3e5dd5cd2
thang duong <thang.duong@cyberlogitec.com>
2024-10-15 17:30:00 +0700

    update google tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics  } from '@next/third-parties/google'
 
 
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <GoogleTagManager gtmId="G-TKBW0Z6X8Y" />
+      <GoogleAnalytics  gaId="G-TKBW0Z6X8Y" />
     HELLO
     </div>
   );
