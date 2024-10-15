1183a65c8dc750b440c00084bd94135c7d2bb054
thang duong <thang.duong@cyberlogitec.com>
2024-10-15 10:19:36 +0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,7 @@ export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
       <GoogleTagManager gtmId="G-TKBW0Z6X8Y" />
-      <div className="w-[95%] h-[20%] bg-white rounded-2xl flex items-center">
-        <form>
-          <div className="flex gap-4">
-          <input type="date" className="text-black" />
-          <input type="date" className="text-black" />
-          </div>
-          <button className="text-black border">
-            submit
-          </button>
-        </form>
-      </div>
+    HELLO
     </div>
   );
 }
