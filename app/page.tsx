114998e71d22216c6abdec67d357a50c2e3366c8
thang duong <thang.duong@cyberlogitec.com>
2024-10-15 10:16:11 +0700

    add google tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
+      <GoogleTagManager gtmId="G-TKBW0Z6X8Y" />
       <div className="w-[95%] h-[20%] bg-white rounded-2xl flex items-center">
         <form>
           <div className="flex gap-4">
